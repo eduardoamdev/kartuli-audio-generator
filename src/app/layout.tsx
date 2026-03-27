70a9a7d2d9180e67085b9a85ea5fbcce256134f9
eduardoamdev <eduardoamdev@gmail.com>
2026-03-27 09:01:40 +0400

    refactor: clean up global styles and simplify Home component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
     </html>
   );
 }
